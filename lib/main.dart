c261472d04bf1eded2c7b5a983b2e62c1bc60257
Amr Ezz <amrezz0111@gmail.com>
2020-08-06 02:36:30 +0200

    adding 7 sounds and 7 colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
+var color = Colors.amber;
+
+Widget layout(var color, var voiceNum) {
+  return Expanded(
+    child: FlatButton(
+      color: color,
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note$voiceNum.wav');
+      },
+      child: Container(
+        child: Center(
+          child: Text('Voice$voiceNum'),
+        ),
+      ),
+    ),
+  );
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
@@ -8,7 +27,18 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              layout(Colors.amber, 1),
+              layout(Colors.blue, 2),
+              layout(Colors.green, 3),
+              layout(Colors.purple, 4),
+              layout(Colors.cyan, 5),
+              layout(Colors.blueGrey, 6),
+              layout(Colors.blueAccent, 7),
+            ],
+          ),
         ),
       ),
     );
